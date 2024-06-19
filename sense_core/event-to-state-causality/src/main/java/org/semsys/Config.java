d4d031c24b8728a47c175ac3868978df3cc6d9fc
Gregor Käfer <gregor.kaefer@tuwien.ac.at>
2024-06-19 13:58:44 +0200

    Change event-to-state-causality config to match the new knowledgebase

--- sense_core/event-to-state-causality/src/main/java/org/semsys/Config.java
+++ sense_core/event-to-state-causality/src/main/java/org/semsys/Config.java
@@ -4,6 +4,6 @@ public class Config {
 
     final String BROKER_PORT = "1883";
     final String BROKER_HOST = "mosquitto";
-    final String REPOSITORY_URL = "http://semantic-event-log:7200/";
-    final String REPOSITORY_NAME = "semantic-event-log";
+    final String REPOSITORY_URL = "http://knowledgebase:7200/";
+    final String REPOSITORY_NAME = "seehub";
 }
