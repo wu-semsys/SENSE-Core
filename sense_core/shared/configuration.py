7926857830fe97efd1a8edb66b4026ec948eba25
Gregor Käfer <gregor.kaefer@tuwien.ac.at>
2024-08-20 10:39:22 +0200

    Update Configs to match the new use case config

--- sense_core/shared/configuration.py
+++ sense_core/shared/configuration.py
@@ -5,14 +5,15 @@ class MqttConfiguration:
     def __init__(self, config: dict) -> None:
         self.host = config["host"]
         self.port = config["port"]
-        self.semantic_event_log_id = config["semantic-event-log-id"]
-        self.simple_event_detection_id = config["simple-event-detection-id"]
-        self.data_ingestion_id = config["data-ingestion-id"]
-        self.event_to_state_causality_id = config["event-to-state-causality-id"]
+        self.semantic_event_log_id = config.get("semantic-event-log-id")
+        self.simple_event_detection_id = config.get("simple-event-detection-id")
+        self.data_ingestion_id = config.get("data-ingestion-id")
+        self.event_to_state_causality_id = config.get("event-to-state-causality-id")
 
     def __str__(self) -> str:
-        return f"Host: {self.host}, Port: {self.port}, Semantic Event Log ID: {self.semantic_event_log_id}, Simple Event Detection ID: {self.simple_event_detection_id}, "
-        f"Data Ingestion ID: {self.data_ingestion_id}, Event to State Causality ID: {self.event_to_state_causality_id}"
+        return (f"Host: {self.host}, Port: {self.port}, Semantic Event Log ID: {self.semantic_event_log_id}, "
+                f"Simple Event Detection ID: {self.simple_event_detection_id}, Data Ingestion ID: {self.data_ingestion_id}, "
+                f"Event to State Causality ID: {self.event_to_state_causality_id}")
 
 
 class GraphDbConfiguration:
@@ -20,7 +21,7 @@ class GraphDbConfiguration:
         self.host = config["host"]
         self.port = config["port"]
         self.repository = config["repository"]
-        self.named_graph = config["named-graph"]
+        self.named_graph = config.get("named-graph")
 
     def __str__(self) -> str:
         return f"Host: {self.host}, Port: {self.port}, Repository: {self.repository}, Named Graph: {self.named_graph}"
