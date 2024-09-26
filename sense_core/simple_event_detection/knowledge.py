b5b7cc7531d8d71ba7d36545266e73bb0ab9cb3e
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2024-09-26 13:42:11 +0200

    Update query for new ontology version

--- sense_core/simple_event_detection/knowledge.py
+++ sense_core/simple_event_detection/knowledge.py
@@ -2,20 +2,22 @@ from typing import List, Tuple
 
 from rdflib import Namespace, URIRef
 from shared.configuration import GraphDbConfiguration
-from shared.graphdb_utils import get_string, get_uri, query_multiple
+from shared.graphdb_utils import get_literal, get_string, get_uri, query_multiple
 from shared.model import (
-    STLEventDetectionProcedure,
-    SignalBoundVariable,
-    Variable,
+    EventDetectionProcedure,
+    LiteralParameterBinding,
+    SignalParameterBinding,
+    ParameterBinding,
 )
 
 SENSE = Namespace("http://w3id.org/explainability/sense#")
+
 
 class MonitoringKnowledgeRepository:
     def __init__(self, graph_db_config: GraphDbConfiguration) -> None:
         self.graph_db_config = graph_db_config
 
-    def find_all_event_detection_procedures(self) -> List[STLEventDetectionProcedure]:
+    def find_all_event_detection_procedures(self) -> List[EventDetectionProcedure]:
         bindings = query_multiple(
             self.graph_db_config,
             """
@@ -23,12 +25,13 @@ class MonitoringKnowledgeRepository:
             PREFIX sosa: <http://www.w3.org/ns/sosa/>
             PREFIX ssn: <http://www.w3.org/ns/ssn/>
 
-            select ?eventDetection ?formula ?sensor ?property ?eventType where { 
-                ?eventDetection rdf:type s:EventDetection ;
-                    s:detectsEventType ?eventType ;
-                    s:hasSTLFormula ?formula .
+            select ?eventDetection ?sensor ?property ?eventType ?method ?definition where { 
+                ?eventDetection a s:EventDetection ;
+                    s:method ?method ;
+                    s:definition ?definition .
                 ?sensor ssn:implements ?eventDetection ;
                     sosa:observes ?property .
+                ?eventType s:detectedBy ?eventDetection ;
             }
             """,
         )
@@ -36,37 +39,62 @@ class MonitoringKnowledgeRepository:
         results = []
         for binding in bindings:
             event_detection = get_uri("eventDetection", binding)
-            formula = get_string("formula", binding)
             sensor = get_uri("sensor", binding)
             property = get_uri("property", binding)
             event = get_uri("eventType", binding)
-            variables = self.__resolve_variables(event_detection)
-            results.append(STLEventDetectionProcedure(event_detection, sensor, property, event, formula, variables))
+            method = get_string("method", binding)
+            definition = get_string("definition", binding)
+            parameter_bindings = self.__resolve_parameter_bindings(event_detection)
+            results.append(
+                EventDetectionProcedure(
+                    event_detection, sensor, property, event, method, definition, parameter_bindings
+                )
+            )
         return results
 
     #
     # Helper Methods
     #
 
-    def __resolve_variables(self, signal_ref: URIRef) -> List[Variable]:
-        variables = []
+    def __resolve_parameter_bindings(self, signal_ref: URIRef) -> List[ParameterBinding]:
+        parameter_bindings = []
 
         signal_bound_variables_result = query_multiple(
             self.graph_db_config,
             f"""
             PREFIX s: <http://w3id.org/explainability/sense#>
 
-            select ?name ?signal ?signal_ref ?signal_ref_type where {{
-                    <{signal_ref}> s:hasVariable ?var .
-                    ?var s:hasName ?name .
-                    ?var s:bindsTo ?signal.
+            select ?name ?sensor where {{
+                    <{signal_ref}> s:hasParameterBinding [
+                        a s:ParameterBinding ;
+                        s:parameterName ?name ;
+                        s:bindsToSensor ?sensor
+                    ] .
             }}
             """,
         )
         for result in signal_bound_variables_result:
             name = get_string("name", result)
-            signal = get_uri("signal", result)
+            signal = get_uri("sensor", result)
+            parameter_bindings.append(SignalParameterBinding(name, signal))
 
-            variables.append(SignalBoundVariable(name, signal))
+        literal_bound_variables_result = query_multiple(
+            self.graph_db_config,
+            f"""
+            PREFIX s: <http://w3id.org/explainability/sense#>
 
-        return variables
+            select ?name ?literal where {{
+                    <{signal_ref}> s:hasParameterBinding [
+                        a s:ParameterBinding ;
+                        s:parameterName ?name ;
+                        s:bindsToLiteral ?literal
+                    ] .
+            }}
+            """,
+        )
+        for result in literal_bound_variables_result:
+            name = get_string("name", result)
+            literal = get_literal("literal", result)
+            parameter_bindings.append(LiteralParameterBinding(name, float(literal)))
+
+        return parameter_bindings
